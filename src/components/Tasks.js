a291b3b74e7a0a50a2781f05de1d15082397ed85
sakhawat246167 <smsiam40@gmail.com>
2022-05-27 12:55:57 +0600

    Initial commit

---
+++ src/components/Tasks.js
@@ -0,0 +1,20 @@
+const tasks = [
+  {
+    id: 1,
+    text: "Doctor Appoinment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "Feb 6th at 1:30pm",
+    reminder: true,
+  },
+];
+
+const Tasks = () => {
+  return <div>Tasks</div>;
+};
+
+export default Tasks;
